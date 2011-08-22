d60a58c81ca3d3a8c2f13e4335a45e2fc9fabae5
Tony Angerilli <tony@angerilli.ca>
2011-08-21 23:33:37 -0700

    Moved the models and sqlalchemy support out of the main server

---
+++ site/models.py
@@ -0,0 +1,40 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker, relationship, backref
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, ForeignKey
+from sqlalchemy.types import String, Integer
+
+from framework import Base
+ 
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    email_address = Column(String)
+ 
+class QueueItem(Base):
+    __tablename__ = 'queue_items'
+    id = Column(Integer, primary_key=True)
+    movie_id = Column(Integer)
+    movie_title = Column(String)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    
+    user = relationship("User", backref=backref('queued_items', order_by=id))
+ 
+    def __init__(self, movie_id, movie_title, user):
+        Base.__init__(self)
+        self.movie_id = movie_id
+        self.movie_title = movie_title
+        self.user = user
+ 
+    def __str__(self):
+        return self.movie_title
+ 
+    def __unicode__(self):
+        return self.movie_title
+ 
+    @staticmethod
+    def list(session):
+        return session.query(QueueItem).all()
+        
+    def to_json(self):
+        return json.dumps({"movie_id":self.movie_id, "movie_title":self.movie_title, "queued":True})
--- site/server.py
+++ site/server.py
@@ -1,127 +1,16 @@
 import sys
 import os
 
+import framework
+from models import QueueItem, User
+
 import cherrypy
-from cherrypy.process import wspbus, plugins
- 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker, relationship, backref
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, ForeignKey
-from sqlalchemy.types import String, Integer
+
 try:
     import json
 except:
-    import simplejson as json
-
-# Helper to map and register a Python class a db table
-Base = declarative_base()
+    import simplejson as json 
  
-class User(Base):
-    __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    email_address = Column(String)
- 
-class QueueItem(Base):
-    __tablename__ = 'queue_items'
-    id = Column(Integer, primary_key=True)
-    movie_id = Column(Integer)
-    movie_title = Column(String)
-    user_id = Column(Integer, ForeignKey('users.id'))
-    
-    user = relationship("User", backref=backref('queued_items', order_by=id))
- 
-    def __init__(self, movie_id, movie_title, user):
-        Base.__init__(self)
-        self.movie_id = movie_id
-        self.movie_title = movie_title
-        self.user = user
- 
-    def __str__(self):
-        return self.movie_title
- 
-    def __unicode__(self):
-        return self.movie_title
- 
-    @staticmethod
-    def list(session):
-        return session.query(QueueItem).all()
-        
-    def to_json(self):
-        return json.dumps({"movie_id":self.movie_id, "movie_title":self.movie_title, "queued":True})
- 
- 
-class SAEnginePlugin(plugins.SimplePlugin):
-    def __init__(self, bus):
-        """
-        The plugin is registered to the CherryPy engine and therefore
-        is part of the bus (the engine *is* a bus) registery.
- 
-        We use this plugin to create the SA engine. At the same time,
-        when the plugin starts we create the tables into the database
-        using the mapped class of the global metadata.
- 
-        Finally we create a new 'bind' channel that the SA tool
-        will use to map a session to the SA engine at request time.
-        """
-        plugins.SimplePlugin.__init__(self, bus)
-        self.sa_engine = None
-        self.bus.subscribe("bind", self.bind)
- 
-    def start(self):
-        db_path = os.path.abspath(os.path.join(os.curdir, 'queues.db'))
-        self.sa_engine = create_engine('sqlite:///%s' % db_path, echo=True)
-        Base.metadata.create_all(self.sa_engine)
- 
-    def stop(self):
-        if self.sa_engine:
-            self.sa_engine.dispose()
-            self.sa_engine = None
- 
-    def bind(self, session):
-        session.configure(bind=self.sa_engine)
- 
-class SATool(cherrypy.Tool):
-    def __init__(self):
-        """
-        The SA tool is responsible for associating a SA session
-        to the SA engine and attaching it to the current request.
-        Since we are running in a multithreaded application,
-        we use the scoped_session that will create a session
-        on a per thread basis so that you don't worry about
-        concurrency on the session object itself.
- 
-        This tools binds a session to the engine each time
-        a requests starts and commits/rollbacks whenever
-        the request terminates.
-        """
-        cherrypy.Tool.__init__(self, 'on_start_resource',
-                               self.bind_session,
-                               priority=20)
- 
-        self.session = scoped_session(sessionmaker(autoflush=True,
-                                                  autocommit=False))
- 
-    def _setup(self):
-        cherrypy.Tool._setup(self)
-        cherrypy.request.hooks.attach('on_end_resource',
-                                      self.commit_transaction,
-                                      priority=80)
- 
-    def bind_session(self):
-        cherrypy.engine.publish('bind', self.session)
-        cherrypy.request.db = self.session
- 
-    def commit_transaction(self):
-        cherrypy.request.db = None
-        try:
-            self.session.commit()
-        except:
-            self.session.rollback()  
-            raise
-        finally:
-            self.session.remove()
-    
 class Queue(object):
     @cherrypy.expose
     def index(self, email_address):
@@ -208,8 +97,11 @@ def setup_routes():
     
 def main(argv):
     # Start the web server
-    SAEnginePlugin(cherrypy.engine).subscribe()
-    cherrypy.tools.db = SATool()
+    db_path = os.path.abspath(os.path.join(os.curdir, 'queues.db'))
+    url = 'sqlite:///%s' % db_path
+    
+    framework.SAEnginePlugin(cherrypy.engine, url).subscribe()
+    cherrypy.tools.db = framework.SATool()
     cherrypy.tree.mount(None, config={'/': {'tools.db.on': True, 'request.dispatch':setup_routes()}})
     cherrypy.engine.start()
     cherrypy.engine.block()
