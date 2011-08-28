76cecc6e58f346e909089cbc4aaf92078facee9c
Tony Angerilli <tony@angerilli.ca>
2011-08-28 14:19:56 -0700

    Added a queue display to the movie page

--- chrome/netflix-queue.js
+++ chrome/netflix-queue.js
@@ -5,13 +5,55 @@ $(function() {
     // TODO: Get user email
     var user_email = "tony@angerilli.ca";
     var movie_url = "http://localhost:8080/users/" + user_email + "/queue/" + movie_id
+    var queue_url = "http://localhost:8080/users/" + user_email + "/queue";
+    
+    // Setup the queue button
     $.getJSON(movie_url, function(data) {
         create_button(data.queued);
     })
     .error(function() { 
+        console.debug("Initial request error");
         create_button(false);
     });
-    
+
+    var queue_dialog = $("<div class='dropdown-menu dropdown-profiles' id='queue_dialog'><ul id='queue_list'></ul></div>");
+    queue_dialog.css("position", "absolute");
+    $("body").append(queue_dialog);
+    queue_dialog.hover(function() {
+        // Need to show the queue as we leave the link and enter the dialog
+        queue_dialog.show();
+    }, function() {
+        queue_dialog.hide();
+    });
+
+    var queue_list_item = $("<li class='last-of-type'></li>");
+
+    // Setup the queue list link
+    queue_link = $("<a href='#'>View Queue</a>").click(function() {
+        pos = $(this).parent().offset();
+        top_pos = pos.top + $(this).innerHeight();
+        left_pos = pos.left + $(this).innerWidth() - 200 + 10;
+        queue_dialog.css("top", top_pos + "px");
+        queue_dialog.css("left", left_pos + "px");
+        // Fetch the queue data
+        $.getJSON(queue_url, function(data) {
+            var list_el = queue_dialog.find("#queue_list");
+            list_el.empty();
+            $.each(data, function(index, queued_item) {
+                li = $("<li><a href='" + queued_item.netflix_url + "'>" + queued_item.movie_title + "</a></li>");
+                list_el.append(li);
+            });
+            queue_dialog.show();
+        });
+    })
+    .hover(function() {}, function() {
+        queue_dialog.hide();
+    });
+
+    queue_list_item.append(queue_link);
+    $("#global-tools ul li:last-child").removeClass('last-of-type');
+    $("#global-tools ul").append(queue_list_item);
+
     function create_button(is_queued) {
         if(is_queued)
         {
